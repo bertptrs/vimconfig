b484c514460950ba524b18380fc0081e474915f7
Bert Peters <bert.ljpeters@gmail.com>
2016-07-12 11:18:28 +0200

    Add more uniform keyboard shortcuts.

--- gsettings.sh
+++ gsettings.sh
@@ -8,3 +8,12 @@ gsettings set org.gnome.desktop.wm.preferences focus-mode "'sloppy'"
 
 # No audible bell
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
+
+# Uniform window controls. Looking at you, Unity.
+gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
+gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Primary><Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Primary><Super>Down']"
+
+# Lock screen with <Super>l
+gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "'<Super>l'"
