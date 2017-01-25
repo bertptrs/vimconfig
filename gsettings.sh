ad3b04e6596c505d998e9d46cfedcbaa5c0f2101
Bert Peters <bert.ljpeters@gmail.com>
2017-01-25 11:31:56 +0100

    Remove unity keybinding overrides.

--- gsettings.sh
+++ gsettings.sh
@@ -9,15 +9,6 @@ gsettings set org.gnome.desktop.wm.preferences focus-mode "'sloppy'"
 # No audible bell
 gsettings set org.gnome.desktop.wm.preferences audible-bell false
 
-# Uniform window controls. Looking at you, Unity.
-gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>Left']"
-gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>Right']"
-gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
-gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>Down']"
-
-# Lock screen with <Super>l
-gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "'<Super>l'"
-
 # No "Move" with <Alt>+F7. Which happens to be "Find usages" in Netbeans
 gsettings set org.gnome.desktop.wm.keybindings begin-move "['']"
 
