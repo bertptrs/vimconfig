4b41e4213b5a48c7f1cb100ee736d8257c52af82
Bert Peters <bert.ljpeters@gmail.com>
2017-03-24 13:38:40 +0100

    No conflict in key bindings anymore.

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -68,9 +68,8 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.spiral,
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
 }
@@ -345,7 +344,7 @@ globalkeys = awful.util.table.join(
 		awful.spawn("xbacklight -inc 15") end),
 
 	-- Lock screen
-	awful.key({ modkey, "Shift" }, "l", function () awful.spawn("gnome-screensaver-command --lock") end),
+	awful.key({ modkey, "Control" }, "l", function () awful.spawn("gnome-screensaver-command --lock") end),
 
 	-- Print screen tools
 	awful.key({ }, "Print", function () awful.spawn("gnome-screenshot") end),
